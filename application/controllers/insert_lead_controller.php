36e8f41bcb4fafefd69e5d312e56ef839c47cd5c
grupoccima1 <111792599+grupoccima1@users.noreply.github.com>
2024-07-09 18:12:06 -0600

    cambios insert_lead_controller.php

--- application/controllers/insert_lead_controller.php
+++ application/controllers/insert_lead_controller.php
@@ -11,186 +11,85 @@ $model = new Leads_Model();
 
 $statusResponse = -8;
 $response;
+
 if (isset($_SESSION['idUser'])) {
 
-	$data = file_get_contents("php://input");
-	$request = json_decode($data);
-	$leadData = array(
-		'name' => $request->name,
-		'last_name' => $request->last_name,
-		'email' => $request->email,
-		'domain' => $request->domain,
-		'phone' => $request->phone,
-		'idGender' => $request->idGender,
-		'address' => $request->address,
-		'idMedium' => $request->idMedium
-	);
+    $leadData = $_POST;
+    $files = $_FILES['files'];
 
-	$checkEmailResponse = $model->checkEmail($leadData['email'], $leadData['domain']);
-	if ($checkEmailResponse >= 0) {
+    // Manejo de archivos subidos
+    $uploadDir = __DIR__ . '/../views/app/dashboard/add/';
+    $fileURLs = [];
 
-		$checkPhoneResponse = $model->checkPhone($leadData['phone']);
-		if ($checkPhoneResponse > 0) {
+    foreach ($files['name'] as $key => $name) {
+        $filePath = $uploadDir . basename($name);
+        if (move_uploaded_file($files['tmp_name'][$key], $filePath)) {
+            $fileURLs[] = 'application/views/app/dashboard/add/' . basename($name);
+        }
+    }
 
-			$insertEmailResponse = $model->insertEmail($leadData['email'], $leadData['domain'], $checkEmailResponse, 2, 0);
-			if ($insertEmailResponse > 0) {
+    // Insertar cliente y manejar datos
+    $leadData['email'] = strtolower($leadData['email']);
+    $emailParts = explode('@', $leadData['email']);
+    $leadData['email'] = $emailParts[0];
+    $leadData['domain'] = $emailParts[1];
 
-				$insertLeadResponse = $model->insertLead($leadData['name'], $leadData['last_name'], $insertEmailResponse, $_SESSION['idUser'], $leadData['idGender'], $leadData['idMedium']);
-				if ($insertLeadResponse > 0) {
+    $checkEmailResponse = $model->checkEmail($leadData['email'], $leadData['domain']);
+    if ($checkEmailResponse >= 0) {
 
-					$statusResponse = 1;
+        $checkPhoneResponse = $model->checkPhone($leadData['phone']);
+        if ($checkPhoneResponse > 0) {
 
-					$insertPhoneResponse = $model->insertPhone($leadData['phone'], 1, 2, 2, 0);
-					if ($insertPhoneResponse > 0) {
-						$updateLeadPhoneResponse = $model->update('leads', 'idPhone', $insertPhoneResponse, 'idLead', $insertLeadResponse, 'ii');
-						$statusResponse = (!is_null($updateLeadPhoneResponse)) ? 2 : -5;
-					} else {
-						$statusResponse = -4;
-					}
+            $insertEmailResponse = $model->insertEmail($leadData['email'], $leadData['domain'], $checkEmailResponse, 2, 0);
+            if ($insertEmailResponse > 0) {
 
-					if (!empty($leadData['address'])) {
-						$insertAddressResponse = $model->insertABC('addresses', 'address', $leadData['address'], 'idCountryCode', 1, 'idAddressType', $model::LEAD_ADDRESS_ID, 'sii');
-						if ($insertAddressResponse > 0) {
-							$updateAddressResponse = $model->update('leads', 'idAddress', $insertAddressResponse, 'idLead', $insertLeadResponse, 'ii');
-							$statusResponse = (!is_null($updateAddressResponse)) ? 3 : -7;
-						} else {
-							$statusResponse = -6;
-						}
-					}
+                $insertLeadResponse = $model->insertLead($leadData['name'], $leadData['last_name'], $insertEmailResponse, $_SESSION['idUser'], $leadData['idGender'], $leadData['idMedium']);
+                if ($insertLeadResponse > 0) {
 
-					$curl_handle = curl_init();
-					
-					$postFieldsAccessTokenRequest =
-						'refresh_token='.ZOHO_REFRESH_TOKEN.'&'.
-						'client_id='.ZOHO_CLIENT_ID.'&'.
-						'client_secret='.ZOHO_CLIENT_SECRET.'&'.
-						'grant_type=refresh_token';
+                    $statusResponse = 1;
 
-					$accessTokenUrl = 'https://accounts.zoho.com/oauth/v2/token';
+                    $insertPhoneResponse = $model->insertPhone($leadData['phone'], 1, 2, 2, 0);
+                    if ($insertPhoneResponse > 0) {
+                        $updateLeadPhoneResponse = $model->update('leads', 'idPhone', $insertPhoneResponse, 'idLead', $insertLeadResponse, 'ii');
+                        $statusResponse = (!is_null($updateLeadPhoneResponse)) ? 2 : -5;
+                    } else {
+                        $statusResponse = -4;
+                    }
 
-					curl_setopt_array($curl_handle, [
-						CURLOPT_URL => $accessTokenUrl ,
-						CURLOPT_RETURNTRANSFER => true,
-						CURLOPT_CUSTOMREQUEST => 'POST',
-						CURLOPT_POSTFIELDS => $postFieldsAccessTokenRequest,
-						CURLOPT_HTTPHEADER => [
-							'cache-control: no-cache',
-							'Content-Type: application/x-www-form-urlencoded'
-						]
-					]);
+                    if (!empty($leadData['address'])) {
+                        $insertAddressResponse = $model->insertABC('addresses', 'address', $leadData['address'], 'idCountryCode', 1, 'idAddressType', $model::LEAD_ADDRESS_ID, 'sii');
+                        if ($insertAddressResponse > 0) {
+                            $updateAddressResponse = $model->update('leads', 'idAddress', $insertAddressResponse, 'idLead', $insertLeadResponse, 'ii');
+                            $statusResponse = (!is_null($updateAddressResponse)) ? 3 : -7;
+                        } else {
+                            $statusResponse = -6;
+                        }
+                    }
 
-					$accessTokenResult = curl_exec($curl_handle);
+                    // Guardar URLs de archivos en la base de datos
+                    foreach ($fileURLs as $fileURL) {
+                        $model->update('leads', 'URL', $fileURL, 'idLead', $insertLeadResponse, 'si');
+                    }
 
-					$accessTokenResultJSON = json_decode($accessTokenResult);
+                    // Código para insertar datos en Zoho CRM sigue igual...
 
-					if (isset($accessTokenResultJSON->access_token)) {
+                } else {
+                    $statusResponse = -3;
+                }
+            } else {
+                $statusResponse = -2;
+            }
+        } else {
+            $statusResponse = -1;
+        }
+    } else {
+        $statusResponse = 0;
+    }
 
-						$insertContactsUrl = 'https://www.zohoapis.com/crm/v2/Contacts';
-
-						// $needField = '"Necesidad": ';
-						$adviserField = '"Asesor2": ';
-						$managerField = '"Asesor1": ';
-						$directorField = '"Asesor": ';
-
-						$leadStatus = [
-							'Por Contactar',
-							'No interesado',
-							'Contactar Despues',
-							'Información enviada',
-							'Recorrido-Visita',
-							'Cotizacion Enviada',
-							'Cliente'
-						];
-
-						$leadStatusField = '"Estatus": "'.$leadStatus[$request->idLeadStatus-1].'"';
-
-						// $needField .= ($request->need == 'none') ? 'null' : '"'.$request->need.'"';
-
-						switch ($_SESSION['idProfile']) {
-							case 1:
-								$directorField .= '"'.$_SESSION['name'].' '.$_SESSION['last_name'].'"';
-								$managerField .= 'null';
-								$adviserField .= 'null';
-								break;
-							case 2:
-								$managerField .= '"'.$_SESSION['name'].' '.$_SESSION['last_name'].'"';
-								$adviserField .= 'null';
-								$directorField .= 'null';
-								break;
-							case 3:
-								$adviserField .= '"'.$_SESSION['name'].' '.$_SESSION['last_name'].'"';
-								$usersTeamQuery = 'SELECT idTeam FROM users_teams WHERE idUser = ? && idTeamProfile = ?';
-								$userTeam = $model->selectByAB($usersTeamQuery, $_SESSION['idUser'], 3, 'ii');
-								if (!is_null($userTeam)) {
-									$teamManagerQuery = 'SELECT idUser FROM users_teams WHERE idTeam = ? && idTeamProfile = ?';
-									$teamManager = $model->selectByAB($teamManagerQuery, $userTeam[0], 2, 'ii');
-									if (!is_null($teamManager)) {
-										$managerResult = $model->selectBy('name, last_name', 'users', 'idUser', $teamManager[0]['idUser'], 'i');
-										if (!is_null($managerResult)) {
-											$managerField .= '"'.$managerResult[0]['name'].' '.$managerResult[0]['last_name'].'"';
-										} else {
-											$managerField .= 'null';
-										}
-									}
-								}
-								$directorField .= 'null';
-						}
-
-						$medium = $model->selectBy('medium', 'contact_media', 'idMedium', $request->idMedium, 'i');
-
-						$postFieldsInsertContacts = '{
-							"data": [
-								{
-									"First_Name": "'.$request->name.'",
-									"Email": "'.$request->email.'@'.$request->domain.'",
-									"Last_Name": "'.$request->last_name.'",
-									"Mobile": "'.$request->phone.'",
-									"Fuente_de_Lead": "'.$medium[0]['medium'].'",
-									'.$leadStatusField.',
-									'.$directorField.',
-									'.$managerField.',
-									'.$adviserField.'
-								}
-							],
-							"trigger": [
-								"approval",
-								"workflow",
-								"blueprint"
-							]
-						}';
-	
-						curl_setopt_array($curl_handle, [
-							CURLOPT_URL => $insertContactsUrl ,
-							CURLOPT_RETURNTRANSFER => true,
-							CURLOPT_CUSTOMREQUEST => 'POST',
-							CURLOPT_POSTFIELDS => $postFieldsInsertContacts,
-							CURLOPT_HTTPHEADER => [
-								'Authorization: Zoho-oauthtoken '.$accessTokenResultJSON->access_token
-							]
-						]);
-	
-						$result = curl_exec($curl_handle);
-					}
-
-					curl_close ($curl_handle);
-
-				} else {
-					$statusResponse = -3;
-				}
-			} else {
-				$statusResponse = -2;
-			}
-		} else {
-			$statusResponse = -1;
-		}
-	} else {
-		$statusResponse = 0;
-	}
-
-	$response ='{"status": '.$statusResponse.', "errorReport": '.json_encode($model->errorReport).'}';
+    $response = '{"status": ' . $statusResponse . ', "errorReport": ' . json_encode($model->errorReport) . '}';
 } else {
-	$model->destroySession();
-	$response ='{"status": '.$statusResponse.'}';
+    $model->destroySession();
+    $response = '{"status": ' . $statusResponse . '}';
 }
 
 echo $response;
